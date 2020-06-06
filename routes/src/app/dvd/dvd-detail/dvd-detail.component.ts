32035d8e5bcc38bd438d57792ee4fb80875966e1
Alexandre Pantalena Yoshimatsu <aleyoshimatsu@gmail.com>
2020-06-06 17:39:12 -0300

    Atualização aula rotas

--- routes/src/app/dvd/dvd-detail/dvd-detail.component.ts
+++ routes/src/app/dvd/dvd-detail/dvd-detail.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, ParamMap, Router } from '@angular/router';
+import { Dvd } from 'src/app/models/dvd';
+import { Observable } from 'rxjs';
+import { DvdService } from 'src/app/services/dvd.service';
 
 @Component({
   selector: 'app-dvd-detail',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DvdDetailComponent implements OnInit {
 
-  constructor() { }
+  dvd$: Observable<Dvd>;
+
+  constructor(private route: ActivatedRoute,
+              private dvdService: DvdService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    let index: number = +this.route.snapshot.paramMap.get('index');
+    this.dvd$ = this.dvdService.get(index);
+    
+    // this.route.paramMap
+    //   .subscribe(
+    //     (param: ParamMap) => console.log("Index: ", param.get('index'))
+    //   )
+  }
+
+  goBack() {
+    this.router.navigate(['/dvds']);
   }
 
 }
